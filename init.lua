ec30d8ec78ec077c2d27498fe58f6ab9d14edb1a
kokage <nakamurahinode@gmail.com>
2024-10-13 22:12:05 +0900

    LSPでバッファをフォーマットするショートカットキーをマッピング

--- init.lua
+++ init.lua
@@ -602,6 +602,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+  nmap('<leader>F', ':Format<CR>', '[F]ormat this buffer')
 end
 
 -- document existing key chains
