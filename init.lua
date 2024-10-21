732959f319606d691153be8be3b4d941bb5db802
kokage <nakamurahinode@gmail.com>
2024-10-21 21:16:39 +0900

    LSP関連のTelescopeについて、ポップアップが出るように変更

--- init.lua
+++ init.lua
@@ -591,12 +591,12 @@ local on_attach = function(_, bufnr)
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>gd', '<cmd>Telescope lsp_difinitions', '[G]oto [D]efinition')
+  nmap('<leader>gr', '<cmd>Telescope lsp_references', '[G]oto [R]eferences')
+  nmap('<leader>gi', '<cmd>Telescope lsp_implementations', '[G]oto [I]mplementation')
+  nmap('<leader>D', '<cmd>Telescope lsp_type_definitions', 'Type [D]efinition')
+  nmap('<leader>ds', '<cmd>Telescope lsp_document_symbols', '[D]ocument [S]ymbols')
+  nmap('<leader>ws', '<cmd>Telescope lsp_dynamic_workspace_symbols', '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
